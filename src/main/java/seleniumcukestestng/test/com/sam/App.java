c42912c93934c8b7856015c8a8cd4419952916e8
sameswark <sameswark@gmail.com>
2015-02-14 23:49:14 +0100

    first commit

---
+++ src/main/java/seleniumcukestestng/test/com/sam/App.java
@@ -0,0 +1,13 @@
+package seleniumcukestestng.test.com.sam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
